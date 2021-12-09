254f5771e9523b98817cd95b0c1a645244422817
sossw1 <sossw1@gmail.com>
2021-12-09 13:59:07 -0500

    mapbox response interface

--- src/api/mapbox.ts
+++ src/api/mapbox.ts
@@ -1,10 +1,40 @@
 import '../env';
 import axios, { AxiosResponse } from 'axios';
 
+interface MapboxResponse {
+  type: string;
+  query: Array<string>;
+  features: Array<{
+    id: string;
+    type: string;
+    place_type: Array<string>;
+    relevance: number;
+    properties: {
+      wikidata: string;
+    };
+    text: string;
+    place_name: string;
+    matching_place_name: string;
+    bbox: Array<number>;
+    center: Array<number>;
+    geometry: {
+      type: string;
+      coordinates: Array<number>;
+    };
+    context: Array<{
+      id: string;
+      wikidata: string;
+      short_code: string;
+      text: string;
+    }>;
+  }>;
+  attribution: string;
+}
+
 export default {
-  request(query: string): Promise<AxiosResponse> {
+  request(query: string): Promise<AxiosResponse<MapboxResponse, string>> {
     const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?access_token=${process.env.MB_API_KEY}&limit=1`;
 
-    return axios.get(url);
+    return axios.get<MapboxResponse>(url);
   }
 };
