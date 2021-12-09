ce55dd1fee97a563673a481525f6e57ae60a1c7d
sossw1 <sossw1@gmail.com>
2021-12-09 13:56:51 -0500

    request return type

--- src/api/mapbox.ts
+++ src/api/mapbox.ts
@@ -1,8 +1,8 @@
 import '../env';
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 
 export default {
-  request(query: string) {
+  request(query: string): Promise<AxiosResponse> {
     const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?access_token=${process.env.MB_API_KEY}&limit=1`;
 
     return axios.get(url);
