14fede3d04b7848139e019ef1cafea5ca8c612db
sossw1 <sossw1@gmail.com>
2021-12-09 15:03:32 -0500

    request geocoding, then use string parsed coordinates in query for weather

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
 import './env';
+import mapbox from './api/mapbox';
+import weatherstack from './api/weatherstack';
+
+const main = async () => {
+  const mapboxData = await mapbox.request('New York, NY');
+  const coordinateString = mapbox.parseCoordinateString(mapboxData);
+  const weatherstackData = await weatherstack.request(coordinateString);
+  console.log(weatherstackData.data);
+};
+
+main();
