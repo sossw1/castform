3de4a784a41f332713c340b6b045ffd3f234040a
sossw1 <sossw1@gmail.com>
2021-12-09 15:02:18 -0500

    units fahrenheit

--- src/api/weatherstack.ts
+++ src/api/weatherstack.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export default {
   request(query: string) {
-    const url = `http://api.weatherstack.com/current?access_key=${process.env.WS_API_KEY}&query=${query}`;
+    const url = `http://api.weatherstack.com/current?access_key=${process.env.WS_API_KEY}&query=${query}&units=f`;
     return axios.get(url);
   }
 };
