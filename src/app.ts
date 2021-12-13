3ab6c47ace7de90ff7a1e0c53d6134a4868594d8
sossw1 <sossw1@gmail.com>
2021-12-13 16:34:38 -0500

    request location

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,11 @@ import mapbox from './api/mapbox';
 import weatherstack from './api/weatherstack';
 
 const main = async () => {
-  const mapboxData = await mapbox.request('New York, NY');
-  const coordinateString = mapbox.parseCoordinateString(mapboxData);
-  const weatherstackData = await weatherstack.request(coordinateString);
-  const currentWeatherData = weatherstackData.data.data.current;
+  const mapboxData = await mapbox.request('Boston, MA');
+  if (mapboxData !== null) {
+    console.log(mapboxData.data.features[0].place_name);
+    const coordinateString = mapbox.parseCoordinateString(mapboxData);
+  }
 };
 
 main();
