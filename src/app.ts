ab814e05669da84496fc89798f10db9691d599bc
sossw1 <sossw1@gmail.com>
2021-12-13 16:36:18 -0500

    log current weather description

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,12 @@ const main = async () => {
     console.log(mapboxData.data.features[0].place_name);
     const coordinateString = mapbox.parseCoordinateString(mapboxData);
     const weatherstackData = await weatherstack.request(coordinateString);
+    if (weatherstackData) {
+      const currentWeather = weatherstackData.data.current;
+      console.log(
+        `${currentWeather.weather_descriptions}. It is currently ${currentWeather.temperature}\xB0F and feels like ${currentWeather.feelslike}\xB0F.`
+      );
+    }
   }
 };
 
