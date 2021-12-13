f675300c6766387a16d567b129576ce44c3149be
sossw1 <sossw1@gmail.com>
2021-12-13 16:41:29 -0500

    command line arg used for location query string

--- src/app.ts
+++ src/app.ts
@@ -3,17 +3,22 @@ import mapbox from './api/mapbox';
 import weatherstack from './api/weatherstack';
 
 const main = async () => {
-  const mapboxData = await mapbox.request('Boston, MA');
-  if (mapboxData !== null) {
-    console.log(mapboxData.data.features[0].place_name);
-    const coordinateString = mapbox.parseCoordinateString(mapboxData);
-    const weatherstackData = await weatherstack.request(coordinateString);
-    if (weatherstackData) {
-      const currentWeather = weatherstackData.data.current;
-      console.log(
-        `${currentWeather.weather_descriptions}. It is currently ${currentWeather.temperature}\xB0F and feels like ${currentWeather.feelslike}\xB0F.`
-      );
+  const locationQuery = process.argv[2];
+  if (locationQuery) {
+    const mapboxData = await mapbox.request(locationQuery);
+    if (mapboxData !== null) {
+      console.log(mapboxData.data.features[0].place_name);
+      const coordinateString = mapbox.parseCoordinateString(mapboxData);
+      const weatherstackData = await weatherstack.request(coordinateString);
+      if (weatherstackData) {
+        const currentWeather = weatherstackData.data.current;
+        console.log(
+          `${currentWeather.weather_descriptions}. It is currently ${currentWeather.temperature}\xB0F and feels like ${currentWeather.feelslike}\xB0F.`
+        );
+      }
     }
+  } else {
+    console.log('No location query entered');
   }
 };
 
